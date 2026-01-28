17c1ebb38b2349c0d5c625ecb3f51aa8b9480788
HuynhDat-svg <huynhdat10a1006@gmail.com>
2026-01-28 13:28:34 +0700

    Version

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import asyncio
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
